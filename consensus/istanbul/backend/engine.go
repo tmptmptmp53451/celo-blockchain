b5c5ac96ae88180b6ae7a448969f672170447a33
Nam Chu Hoai <nambrot@googlemail.com>
2019-03-27 16:30:04 -0700

    Log null block

--- consensus/istanbul/backend/engine.go
+++ consensus/istanbul/backend/engine.go
@@ -435,6 +435,7 @@ func (sb *Backend) Seal(chain consensus.ChainReader, block *types.Block, results
 		for {
 			select {
 			case result := <-sb.commitCh:
+				log.Error("aminull", "block", block, "result", result)
 				// if the block hash and the hash from channel are the same,
 				// return the result. Otherwise, keep waiting the next hash.
 				if block.Hash() == result.Hash() {
