94a142a151cf91eff3b3f24afcf69abf99f470e0
Nam Chu Hoai <nambrot@googlemail.com>
2019-04-22 07:48:17 -0700

    higher limits

--- consensus/istanbul/backend/engine.go
+++ consensus/istanbul/backend/engine.go
@@ -42,7 +42,7 @@ const (
 	checkpointInterval = 1024 // Number of blocks after which to save the vote snapshot to the database
 	inmemorySnapshots  = 128  // Number of recent vote snapshots to keep in memory
 	inmemoryPeers      = 300
-	inmemoryMessages   = 23000
+	inmemoryMessages   = 43000
 )
 
 var (
