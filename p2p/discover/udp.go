ceb96cdb9caf4a38860d7b6988d0f714c4296885
Nam Chu Hoai <nambrot@googlemail.com>
2019-06-21 12:52:46 +0200

    Use IP from ping package

--- p2p/discover/udp.go
+++ p2p/discover/udp.go
@@ -658,9 +658,10 @@ func (req *ping) handle(t *udp, from *net.UDPAddr, fromID enode.ID, mac []byte) 
 	})
 
 	// Ping back if our last pong on file is too far in the past.
-	n := wrapNode(enode.NewV4(req.senderKey, from.IP, int(req.From.TCP), from.Port))
-	if time.Since(t.db.LastPongReceived(n.ID(), from.IP)) > bondExpiration {
-		t.sendPing(fromID, from, func() {
+	n := wrapNode(enode.NewV4(req.senderKey, req.From.IP, int(req.From.TCP), from.Port))
+	if time.Since(t.db.LastPongReceived(n.ID(), req.From.IP)) > bondExpiration {
+		fromAddr := net.UDPAddr{IP: req.From.IP, Port: int(req.From.UDP)}
+		t.sendPing(fromID, &fromAddr, func() {
 			t.tab.addVerifiedNode(n)
 		})
 	} else {
