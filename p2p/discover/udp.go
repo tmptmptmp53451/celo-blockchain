1067f573ceabbdbcffb2f853ece0c616d60c8ce7
Nam Chu Hoai <nambrot@googlemail.com>
2019-06-25 12:44:32 +0200

    Use passed UDP port

--- p2p/discover/udp.go
+++ p2p/discover/udp.go
@@ -666,7 +666,7 @@ func (req *ping) handle(t *udp, from *net.UDPAddr, fromID enode.ID, mac []byte) 
 	})
 
 	// Ping back if our last pong on file is too far in the past.
-	n := wrapNode(enode.NewV4(req.senderKey, senderFrom, int(req.From.TCP), from.Port))
+	n := wrapNode(enode.NewV4(req.senderKey, senderFrom, int(req.From.TCP), int(req.From.UDP)))
 	if time.Since(t.db.LastPongReceived(n.ID(), from.IP)) > bondExpiration {
 		t.sendPing(fromID, from, func() {
 			t.tab.addVerifiedNode(n)
