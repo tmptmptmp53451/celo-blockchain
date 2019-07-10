6facd0a843a1df82edaaa137bef8a3d3fd6752ad
Nam Chu Hoai <nambrot@googlemail.com>
2019-07-10 15:15:54 +0200

    Do not use lan ip addresses

--- p2p/discover/udp.go
+++ p2p/discover/udp.go
@@ -652,7 +652,7 @@ func (req *ping) preverify(t *udp, from *net.UDPAddr, fromID enode.ID, fromKey e
 func (req *ping) handle(t *udp, from *net.UDPAddr, fromID enode.ID, mac []byte) {
 	// Reply.
 	senderFrom := from.IP
-	if req.From.IP != nil && !req.From.IP.IsLoopback() {
+	if req.From.IP != nil && !req.From.IP.IsLoopback() && !netutil.IsLAN(req.From.IP) {
 		senderFrom = req.From.IP
 	}
 
