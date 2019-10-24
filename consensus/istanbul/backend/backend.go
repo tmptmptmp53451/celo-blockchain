e6c4daa0aa153b2614f743a5f745b4341c5f2c59
Trevor Porter <trkporter@ucdavis.edu>
2019-10-24 00:34:23 -0700

    Revert "Most tests passing (#512)"
    This reverts commit 8ca3de7d8784ce4d96e5bf4b588686b2292732db.

--- consensus/istanbul/backend/backend.go
+++ consensus/istanbul/backend/backend.go
@@ -29,7 +29,6 @@ import (
 	"github.com/ethereum/go-ethereum/consensus/istanbul"
 	istanbulCore "github.com/ethereum/go-ethereum/consensus/istanbul/core"
 	"github.com/ethereum/go-ethereum/consensus/istanbul/validator"
-	"github.com/ethereum/go-ethereum/contract_comm/election"
 	"github.com/ethereum/go-ethereum/contract_comm/random"
 	"github.com/ethereum/go-ethereum/contract_comm/validators"
 	"github.com/ethereum/go-ethereum/core"
@@ -378,15 +377,6 @@ func (sb *Backend) Verify(proposal istanbul.Proposal) (time.Duration, error) {
 	return 0, err
 }
 
-func (sb *Backend) getNewValidatorSet(header *types.Header, state *state.StateDB) ([]istanbul.ValidatorData, error) {
-	newValSetAddresses, err := election.GetElectedValidators(header, state)
-	if err != nil {
-		return nil, err
-	}
-	newValSet, err := validators.GetValidatorData(header, state, newValSetAddresses)
-	return newValSet, err
-}
-
 func (sb *Backend) validatorRandomnessAtBlockNumber(number uint64, hash common.Hash) (common.Hash, error) {
 	header := sb.chain.GetHeader(hash, number)
 	if header == nil {
@@ -408,7 +398,7 @@ func (sb *Backend) verifyValSetDiff(proposal istanbul.Proposal, block *types.Blo
 		return err
 	}
 
-	newValSet, err := sb.getNewValidatorSet(block.Header(), state)
+	newValSet, err := validators.GetValidatorSet(block.Header(), state)
 	if err != nil {
 		log.Error("Istanbul.verifyValSetDiff - Error in retrieving the validator set. Verifying val set diff empty.", "err", err)
 		if len(istExtra.AddedValidators) != 0 || istExtra.RemovedValidators.BitLen() != 0 {
