7e36f34c9b1893afbe0bdc60c67390514ab493d4
Trevor Porter <trkporter@ucdavis.edu>
2019-12-09 14:47:05 -0800

    Don't force light servers to use discv5

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -1050,10 +1050,10 @@ func SetP2PConfig(ctx *cli.Context, cfg *p2p.Config) {
 		cfg.UseInMemoryNodeDatabase = true
 	}
 
-	// if we're running a light client or server, force enable the v5 peer discovery
+	// if we're running a light client, force enable the v5 peer discovery
 	// unless it is explicitly disabled with --nodiscover note that explicitly specifying
 	// --v5disc overrides --nodiscover, in which case the later only disables v4 discovery
-	forceV5Discovery := (lightClient || lightServer) && !ctx.GlobalBool(NoDiscoverFlag.Name)
+	forceV5Discovery := lightClient && !ctx.GlobalBool(NoDiscoverFlag.Name)
 	if ctx.GlobalIsSet(DiscoveryV5Flag.Name) {
 		cfg.DiscoveryV5 = ctx.GlobalBool(DiscoveryV5Flag.Name)
 	} else if forceV5Discovery {
